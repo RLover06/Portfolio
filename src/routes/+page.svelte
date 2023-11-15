724963c062c6a5629b7a3f5b63f6cc30890bafb0
Hunter Stevens <hunter@toplobster.io>
2023-11-15 16:43:39 -0500

    Ported over files and assets

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Header from './header.svelte';
+  import Hero from './hero.svelte';
+  import Letter from './letter.svelte';
+  import TechStack from './techStack.svelte';
+  import Products from './products.svelte';
+  import LaunchSchool from './launchSchool.svelte';
+  import Contact from './contact.svelte';
+  import Favorites from './favorites.svelte';
+  import Footer from './footer.svelte';
+
+  import { onMount } from 'svelte';
+
+  let ready = false;
+
+  onMount(() => {
+    ready = true;
+  })
+</script>
+
+{#if ready}
+  <Header/>
+  <Hero/>
+  <Letter/>
+  <TechStack/>
+  <Products/>
+  <LaunchSchool/>
+  <Contact/>
+  <Favorites/>
+  <Footer/>
+{/if}
