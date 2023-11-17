a8ac308f597219dc1b6266413094279871517354
Hunter Stevens <hunter@toplobster.io>
2023-11-17 16:26:45 -0500

    Remove mobile header and update summary intro

--- src/routes/hero.svelte
+++ src/routes/hero.svelte
@@ -2,23 +2,23 @@
 	import { blur } from 'svelte/transition';
 </script>
 
-<div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+<div class="mt-10 mx-auto max-w-screen-xl px-2 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
 	<div class="text-center">
-		<div transition:blur={{ duration: 1500 }} class="image-wrapper relative h-96 w-96 mx-auto p-4 rounded-full">
+		<div transition:blur={{ duration: 1500 }} class="image-wrapper relative w-3/4 md:w-1/3 h-auto mx-auto p-4 rounded-full">
 			<img src="/headshot.jpeg" alt="" class="h-full w-full object-cover rounded-full glow-box" />
 		</div>
 
 		<h2 transition:blur={{ duration: 2000 }} class="mt-10 text-4xl tracking-tight leading-10 font-extrabold text-gray-100 sm:text-5xl sm:leading-none md:text-6xl drop-shadow-lg glow">
 			Hunter Stevens
-			<br class="md:block" />
-			<span transition:blur={{ duration: 3000 }} class="bg-gradient-to-r text-indigo-300">Full-Stack Developer</span>
+			<br />
+			<span transition:blur={{ duration: 3000 }} class="whitespace-nowrap bg-gradient-to-r text-indigo-300">Full-Stack Developer</span>
 		</h2>
 
 		<div class="mt-3 max-w-md mx-auto text-base text-gray-400 sm:text-xl md:mt-5 md:text-xl md:max-w-3xl">
 			<div transition:blur={{ duration: 3500 }}>
 				Education:
 				<a href="https://launchschool.com/courses" target="_blank">
-					<span class="font-semibold text-green-400 italic"> University of Houston (BBA in Marketing) </span>
+					<span class="font-semibold text-green-400 italic"> University of Houston <span class="hidden md:block">(BBA in Marketing)</span> </span>
 				</a>
 				<br />
 			</div>
@@ -27,7 +27,7 @@
 				Certifications:
 				<a href="https://launchschool.com/courses" target="_blank">
 					<span class="font-semibold text-green-300 italic">
-						Launch School <span class="font-light">(Core Curriculum)</span>
+						Launch School <span class="hidden md:block">(Core Curriculum)</span>
 					</span>
 				</a>
 			</div>
@@ -37,7 +37,7 @@
 			</div>
 
 			<div transition:blur={{ duration: 5000 }}>
-				Learning: <span class="font-semibold text-green-300">React</span>
+				Learning: <span class="font-semibold text-green-300">Playwright</span>
 			</div>
 		</div>
 	</div>
