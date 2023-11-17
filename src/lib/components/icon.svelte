008bd6bc3150586d0f1e5bf6e016a72f3a2b38c8
Hunter Stevens <hunter@toplobster.io>
2023-11-17 10:13:02 -0500

    Create Icon reusable component

---
+++ src/lib/components/icon.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let linkURL = '';
+	export let imgURL = '';
+</script>
+
+<a href={linkURL}>
+	<img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src={imgURL} alt="" />
+</a>
--- src/routes/techStack.svelte
+++ src/routes/techStack.svelte
@@ -1,60 +1,31 @@
+<script>
+	import Icon from '$lib/components/icon.svelte';
+</script>
+
 <div class="bg-slate-950 py-8" id="skills">
-  <div class="">
-    <h2 class="text-3xl font-bold tracking-tight text-center sm:text-4xl bg-gradient-to-r text-indigo-300 glow">Technologies Used</h2>
-    <!-- Logo Cloud Row -->
-      <div class="bg-slate-950">
-        <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
-          <div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-8">
-            <a href="https://www.typescriptlang.org/">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="">
-            </a>
-            <a href="https://en.wikipedia.org/wiki/JavaScript">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="">
-            </a>
-            <a href="https://nodejs.org/en">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="">
-            </a>
-            <a href="https://react.dev/">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="">
-            </a>
-            <a href="https://svelte.dev/">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg" alt="">
-            </a>
-            <a href="https://en.wikipedia.org/wiki/MongoDB">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="">
-            </a>
-            <a href="https://firebase.google.com/">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" alt="">
-            </a>
-            <a href="https://www.postgresql.org/">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt="">
-            </a>
-            <a href="https://en.wikipedia.org/wiki/HTML5">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="">
-            </a>
-            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="">
-            </a>
-            <a href="https://tailwindcss.com/">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" alt="">
-            </a>
-            <a href="https://jestjs.io/">
-              <img class="h-14 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="">
-            </a>
-            <a href="https://en.wikipedia.org/wiki/Heroku">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" alt="">
-            </a>
-            <a href="https://en.wikipedia.org/wiki/Google_Cloud_Platform">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg" alt="">
-            </a>
-            <a href="https://stripe.com/">
-              <img class="h-12 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://www.solodev.com/file/2e7ff4de-48b3-11ed-bb2e-0eaef3759f5f/stripe-icon.png" alt="">
-            </a>
-            <a href="https://www.algolia.com/">
-              <img class="h-14 col-span-1 mx-auto flex justify-center shake-on-hover" src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/12_Algolia_logo_logos-512.png" alt="">
-            </a>
-          </div>
-          </div>
-        </div>
-      </div>
-  </div>
+	<div class="">
+		<h2 class="text-3xl font-bold tracking-tight text-center sm:text-4xl bg-gradient-to-r text-indigo-300 glow">Technologies Used</h2>
+		<div class="bg-slate-950">
+			<div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
+				<div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-8">
+					<Icon linkURL="https://www.typescriptlang.org/" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
+					<Icon linkURL="https://en.wikipedia.org/wiki/JavaScript" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+					<Icon linkURL="https://nodejs.org/en" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+					<Icon linkURL="https://react.dev/" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+					<Icon linkURL="https://svelte.dev/" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg" />
+					<Icon linkURL="https://en.wikipedia.org/wiki/MongoDB" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+					<Icon linkURL="https://firebase.google.com/" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" />
+					<Icon linkURL="https://www.postgresql.org/" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+					<Icon linkURL="https://en.wikipedia.org/wiki/HTML5" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+					<Icon linkURL="https://developer.mozilla.org/en-US/docs/Web/CSS" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+					<Icon linkURL="https://tailwindcss.com/" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
+					<Icon linkURL="https://jestjs.io/" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
+					<Icon linkURL="https://en.wikipedia.org/wiki/Heroku" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" />
+					<Icon linkURL="https://en.wikipedia.org/wiki/Google_Cloud_Platform" imgURL="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg" />
+					<Icon linkURL="https://stripe.com/" imgURL="https://www.solodev.com/file/2e7ff4de-48b3-11ed-bb2e-0eaef3759f5f/stripe-icon.png" />
+					<Icon linkURL="https://www.algolia.com/" imgURL="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/12_Algolia_logo_logos-512.png" />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
