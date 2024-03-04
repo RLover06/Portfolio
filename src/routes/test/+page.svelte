458593e23aaf3435665c29f0ace5cb4ef2cabb8e
Hunter Stevens <hunter@toplobster.io>
2024-03-04 18:27:53 -0500

    Added testing for Chatnode

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,10 @@
+<iframe src="https://embed.chatnode.ai/b005e7c77daeb53f" width="100%" height="700" style="visibility: hidden; border: none" onload="this.style.visibility='visible';" allow="clipboard-read; clipboard-write" />
+
+<!-- <iframe
+			src="https://dev.embed.chatnode.ai/aea8e2320127d799"
+			width="100%"
+			height="700"
+			style="visibility: hidden; border: none"
+			onload="this.style.visibility='visible';"
+			allow="clipboard-read; clipboard-write"
+		></iframe> -->
