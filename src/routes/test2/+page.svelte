f00c60334f07c780ae8fdfee86c72287e7a5c1b3
Hunter Stevens <hunter@toplobster.io>
2024-03-06 13:46:45 -0500

    Update test pages

--- src/routes/test2/+page.svelte
+++ src/routes/test2/+page.svelte
@@ -1,4 +1,14 @@
+<script>
+	import Letter from '../letter.svelte';
+</script>
+
 <svelte:head>
 	<meta name="robots" content="noindex,nofollow" />
-	<script src="https://dev.embed.chatnode.ai/b3bb9620cd721a3b/popup.js"></script>
+	<script src="https://dev.embed.chatnode.ai/de3c08d9c1fff938/popup.js"></script>
 </svelte:head>
+
+<Letter />
+<Letter />
+<Letter />
+<Letter />
+<Letter />
