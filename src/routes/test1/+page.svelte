6f29317426d68a34ba9fa66b50d6da69e3a3d471
Hunter Stevens <hunter@toplobster.io>
2024-03-09 10:37:09 -0500

    Removed chatbot from homepage

--- src/routes/test1/+page.svelte
+++ src/routes/test1/+page.svelte
@@ -1,4 +1,4 @@
 <svelte:head>
 	<meta name="robots" content="noindex,nofollow" />
-	<script src="https://dev.embed.chatnode.ai/2bd405379535aca5/popup.js"></script>
+	<script src="https://dev.embed.chatnode.ai/61c202b5663e5e8a/popup.js"></script>
 </svelte:head>
