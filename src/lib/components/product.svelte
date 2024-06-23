c8a11d1efedd49056fdabed7eaacbc213bc62304
Hunter Stevens <hunter@toplobster.io>
2024-06-23 13:45:45 -0500

    chore: add products as employee

--- src/lib/components/product.svelte
+++ src/lib/components/product.svelte
@@ -16,7 +16,7 @@
 	</div>
 	<div class="max-w-xl">
 		<div class="mt-8 mb-2 flex items-center gap-x-4 text-xs">
-			<span class="my-1 text-lg bg-gradient-to-r from-green-600 via-green-400 to-green-400 text-transparent bg-clip-text">{category}</span>
+			<span class="my-1 text-lg bg-gradient-to-r from-slate-600 via-slate-400 to-slate-400 text-transparent bg-clip-text"><span class="text-gray-600">(role) </span>{category}</span>
 		</div>
 		<div class="group relative">
 			<h3 class="mt-3 text-2xl font-semibold leading-6 bg-gradient-to-r from-indigo-500 via-sky-200 to-white text-transparent bg-clip-text">
--- src/routes/products.svelte
+++ src/routes/products.svelte
@@ -42,6 +42,29 @@
 		{ name: 'Contentful', linkURL: 'https://www.contentful.com/', imgURL: 'contentful.png' },
 		{ name: 'Vercel', linkURL: 'https://vercel.com/', imgURL: 'vercel.png' }
 	];
+
+	const chat_node_icons = [
+		{ name: 'TypeScript', linkURL: 'https://www.typescriptlang.org/', imgURL: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' },
+		{ name: 'Svelte', linkURL: 'https://svelte.dev/', imgURL: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg' },
+		{ name: 'Vite.js', linkURL: 'https://vitejs.dev/', imgURL: 'vite.png' },
+		{ name: 'Supabase', linkURL: 'https://supabase.com', imgURL: 'supabase.png' },
+		{ name: 'PostgreSQL', linkURL: 'https://www.postgresql.org/', imgURL: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg' },
+		{ name: 'Prisma', linkURL: 'https://www.prisma.io/', imgURL: 'prisma.png' },
+		{ name: 'Tailwind CSS', linkURL: 'https://tailwindcss.com/', imgURL: 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/tailwindcss/tailwindcss-original.svg' },
+		{ name: 'Stripe', linkURL: 'https://stripe.com/', imgURL: 'https://www.solodev.com/file/2e7ff4de-48b3-11ed-bb2e-0eaef3759f5f/stripe-icon.png' },
+		{ name: 'Lucia Auth', linkURL: 'https://lucia-auth.com/', imgURL: 'lucia.png' },
+		{ name: 'Vercel', linkURL: 'https://vercel.com/', imgURL: 'vercel.png' }
+	];
+
+	const mlytica_icons = [
+		{ name: 'TypeScript', linkURL: 'https://www.typescriptlang.org/', imgURL: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' },
+		{ name: 'Svelte', linkURL: 'https://svelte.dev/', imgURL: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg' },
+		{ name: 'Vite.js', linkURL: 'https://vitejs.dev/', imgURL: 'vite.png' },
+		{ name: 'Docker', linkURL: 'https://www.docker.com/', imgURL: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg' },
+		{ name: 'Shadcn UI', linkURL: 'https://ui.shadcn.com/', imgURL: '/shadcn-ui.png' },
+		{ name: 'Tailwind CSS', linkURL: 'https://tailwindcss.com/', imgURL: 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/tailwindcss/tailwindcss-original.svg' },
+	];
+
 </script>
 
 <div id="products" class="bg-slate-950 py-12 sm:py-16">
@@ -54,7 +77,7 @@
 		<!-- Products -->
 		<div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 			<Product
-				category="Mailing Lists"
+				category="Founder"
 				title="AirMailer.io"
 				description="AirMailer.io allows marketers to generate mailing list with just a few clicks. I like to think of it as a vending machine for addresses. (94 Million Addresses)"
 				linkURL="https://airmailer.io"
@@ -63,7 +86,7 @@
 			/>
 
 			<Product
-				category="Homeowner Data"
+				category="Creator"
 				title="DataForest.io"
 				description="DataForest.io allows marketers to market geotarget homeowners. Think, Google Maps for homeowner + propery data."
 				linkURL="https://dataforest.io"
@@ -72,13 +95,43 @@
 			/>
 
 			<Product
-				category="Language Learning"
+				category="Creator"
 				title="VerticalSpanish.com"
 				description="Vertical Spanish allows Spanish students to study spanish verbs in a world that inspires. Conjugators are notorious for being overpopulated with ads, but not here!"
 				linkURL="https://verticalspanish.com"
 				imgURL="landscape.png"
 				icons={vertical_spanish_icons}
 			/>
+
+			<!-- Continue for Chatnode.ai -->
+
+			<Product
+				category="Full-Stack Developer"
+				title="Chatnode.ai"
+				description="I thoroughly enjoyed my role in the full-stack development of both the chatbot (core product) and client dashboard at ChatNode."
+				linkURL="https://chatnode.ai"
+				imgURL="chatnode_ss.png"
+				icons={chat_node_icons}
+			/>
+
+			<!-- Contiue for MLytica -->
+			 <Product
+			 		category="Frontend Developer"
+					title="Merlin @ MLytica"
+					description="Merlin is a powerful AI tool that automates customer support emails, giving superpowers to customer support agents. Merlin is a product of MLytica."
+					linkURL="https://mlytica.com"
+					imgURL="merlin_ss.png"
+					icons={mlytica_icons}
+				/>		
+
+				<Product
+				category="Frontend Developer"
+			 title="Genie @ MLytica"
+			 description="Genie is your personal AI assistant and can fill in many roles to help in a variety of departments and teams as well as individuals. Genie is a product of MLytica."
+			 linkURL="https://mlytica.com"
+			 imgURL="genie_ss.png"
+			 icons={mlytica_icons}
+		 />	
 		</div>
 	</div>
 </div>
