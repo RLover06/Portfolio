2ed3581ba220f7175f13b89750cd3b313676538a
Hunter Stevens <hunter@toplobster.io>
2024-09-03 17:42:20 -0500

    feat: hover image

--- src/routes/hero.svelte
+++ src/routes/hero.svelte
@@ -1,17 +1,22 @@
-<script>
+<script lang="ts">
 	import { blur } from 'svelte/transition';
+
+	let image = '/hunter-coco.jpg';
 </script>
 
 <div class="mt-10 mx-auto max-w-screen-xl px-2 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
 	<div class="text-center">
-		<div transition:blur={{ duration: 1500 }} class="image-wrapper relative w-3/4 md:w-1/3 h-auto mx-auto p-4 rounded-full">
-			<img src="/headshot.jpeg" alt="" class="h-full w-full object-cover rounded-full glow-box" />
+		<div transition:blur={{ duration: 1500 }} class="image-wrapper relative w-3/4 md:w-1/3 h-auto mx-auto p-4 rounded-full group">
+			<img src="/headshot.jpeg" alt="" class="h-full w-full object-cover rounded-full glow-box group-hover:hidden" />
+			<img src="/hunter-coco.jpg" alt="" class="h-full w-full object-cover rounded-full glow-box hidden group-hover:block" />
 		</div>
+
+		
 
 		<h2 transition:blur={{ duration: 2000 }} class="mt-10 text-4xl tracking-tight leading-10 font-extrabold text-gray-100 sm:text-5xl sm:leading-none md:text-6xl drop-shadow-lg glow space-grotesk">
 			Hunter Stevens
 			<br />
-			<span transition:blur={{ duration: 3000 }} class="whitespace-nowrap bg-gradient-to-r text-indigo-300 space-grotesk">Full-Stack Developer</span>
+			<span transition:blur={{ duration: 3000 }} class="whitespace-nowrap bg-gradient-to-r text-indigo-300 space-grotesk">Software Developer</span>
 		</h2>
 
 		<div class="mt-3 max-w-md mx-auto text-base text-neutral-600 sm:text-xl md:mt-5 md:text-xl md:max-w-3xl">
