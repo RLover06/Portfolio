fe5fce7170ac831e6ebbe4abc6c01cb886efaf07
Hunter Stevens <hunter@toplobster.io>
2024-10-14 15:40:34 -0500

    feat: testimonials

--- src/routes/testimonials.svelte
+++ src/routes/testimonials.svelte
@@ -1,33 +1,32 @@
-<div class="relative isolate pb-32 pt-24 sm:pt-32">
+<div class="relative isolate pb-16 pt-24 sm:pt-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-xl text-center">
       <h2 class="text-3xl font-bold tracking-tight text-center sm:text-4xl bg-gradient-to-r text-indigo-300 glow  space-grotesk">Testimonials</h2>
     </div>
     <div class="mx-auto mt-8 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
-      <figure class="rounded-2xl bg-gradient-to-tl from-slate-100 to-neutral-400 shadow-lg ring-1 ring-gray-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
-        <blockquote class="p-6 text-lg font-semibold leading-7 tracking-tight text-gray-900 sm:p-12 sm:text-xl sm:leading-8">
-          <p>“Hunter was awesome to work with. He played a huge role in building out the chatbot and really nailed the dashboard design, making everything easy to use. Super collaborative and always had great ideas. ”</p>
+      <figure class="rounded-2xl bg-gradient-to-tl from-slate-800 to-slate-600 shadow-lg ring-1 ring-gray-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
+        <blockquote class="p-6 text-lg font-semibold leading-7 tracking-tight text-gray-400 sm:p-12 sm:text-xl sm:leading-8">
+          <p>“Hunter has been great to work with on the ChatNode web app. He’s reliable, quick to solve problems, and always brings a lot of thought to the project. He has great communication skills too! It’s been awesome to have him on the team, and he’s helped bring many features to ChatNode.”</p>
         </blockquote>
         <figcaption class="flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-gray-900/10 px-6 py-4 sm:flex-nowrap">
           <img class="h-10 w-10 flex-none rounded-full bg-gray-50" src="sacha.jpg" alt="">
           <div class="flex-auto">
-            <div class="font-semibold">Sacha Dumay</div>
+            <div class="font-semibold text-gray-300">Sacha Dumay</div>
           </div>
-          <div class="font-semibold">Chatnode.ai</div>
-          <!-- <img class="h-10 w-auto flex-none" src="/logo-chatnode.png" alt=""> -->
+          <div class="font-semibold text-gray-300">Chatnode.ai</div>
         </figcaption>
       </figure>
       <div class="space-y-8 xl:contents xl:space-y-0">
         <div class="space-y-8 xl:row-span-2">
-          <figure class="rounded-2xl bg-gradient-to-tl from-slate-100 to-neutral-400 p-6 shadow-lg ring-1 ring-gray-900/5">
-            <blockquote class="text-gray-900  font-semibold text-base">
+          <figure class="rounded-2xl bg-gradient-to-tl from-slate-800 to-slate-600 p-6 shadow-lg ring-1 ring-gray-900/5">
+            <blockquote class="text-gray-400  font-semibold text-base">
               <p>“We had a pleasure working together and successfully completed the project. Hunter is skilled proffessional who adhered to our requirements and delivered great results.”</p>
             </blockquote>
             <figcaption class="mt-6 flex items-center gap-x-4">
               <img class="h-10 w-10 rounded-full bg-gray-50" src="/michael.png" alt="">
               <div>
-                <div class="font-semibold">Michael Makedonsky</div>
-                <div class="">Agatha Global Tech</div>
+                <div class="font-semibold text-gray-300">Michael Makedonsky</div>
+                <div class="text-gray-300">Agatha Global Tech</div>
               </div>
             </figcaption>
           </figure>
@@ -37,23 +36,27 @@
       </div>
       <div class="space-y-8 xl:contents xl:space-y-0">
         <div class="space-y-8 xl:row-span-2">
-          <figure class="rounded-2xl bg-gradient-to-tl from-slate-100 to-neutral-400 p-6 shadow-lg ring-1 ring-gray-900/5">
-            <blockquote class="text-gray-900 font-semibold text-base">
+          <figure class="rounded-2xl bg-gradient-to-tl from-slate-800 to-slate-600 p-6 shadow-lg ring-1 ring-gray-900/5">
+            <blockquote class="text-gray-400 font-semibold text-base">
               <p>“Hunter was an absolute pleasure working with. He has very strong work ethics, very responsive and communicative. He's done a great job and fit in really well with the team. ”</p>
             </blockquote>
             <figcaption class="mt-6 flex items-center gap-x-4">
               <img class="h-10 w-10 rounded-full bg-gray-50" src="josh.jpg" alt="">
               <div>
-                <div class="font-semibold">Josh Yang</div>
-                <div class="">MLytica</div>
+                <div class="font-semibold text-gray-300">Josh Yang</div>
+                <div class=" text-gray-300">MLytica</div>
               </div>
             </figcaption>
           </figure>
 
           <!-- More testimonials... -->
+
         </div>
       </div>
     </div>
   </div>
 </div>
+
+
+
 
