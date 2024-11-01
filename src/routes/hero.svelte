e9985e8b32abf8794b9eaec4a524379353db8b4e
Hunter Stevens <hunter@toplobster.io>
2024-11-01 12:43:50 -0500

    chore: update spacing

--- src/routes/hero.svelte
+++ src/routes/hero.svelte
@@ -53,13 +53,13 @@
 					<path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
 				</svg>
 				<div class="flex gap-2 items-center">
-					<a href="https://www.youtube.com/@HunterScript/featured" class=" py-0.5 px-1.5 bg-white/20 rounded-xl border-2 border-gray-700 hover:bg-white/10">
+					<a href="https://www.youtube.com/@HunterScript/featured" class="px-1.5 bg-white/20 rounded-xl border-2 border-gray-700 hover:bg-white/10">
 						<button class="font-semibold bg-gradient-to-b from-slate-100 via-neutral-300 to-slate-600 text-transparent bg-clip-text">
 							@HunterScript
 						</button>
 					</a>
 
-					<a href="https://www.youtube.com/@VerticalSpanish/featured" class=" py-0.5 px-1.5 bg-white/20 rounded-xl border-2 border-gray-700 hover:bg-white/10">
+					<a href="https://www.youtube.com/@VerticalSpanish/featured" class="px-1.5 bg-white/20 rounded-xl border-2 border-gray-700 hover:bg-white/10">
 						<button class="font-semibold bg-gradient-to-b from-slate-100 via-neutral-300 to-slate-600 text-transparent bg-clip-text">
 							@VerticalSpanish
 						</button>
