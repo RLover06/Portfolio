84b19466627e3447d9e4f77dfb1af305146850a1
Hunter Stevens <hunter@toplobster.io>
2024-11-03 17:09:02 -0500

    fix: hide 2nd column on mobile

--- src/lib/components/favorite.svelte
+++ src/lib/components/favorite.svelte
@@ -7,7 +7,7 @@
 <li>
 	<div class="flex gap-3">
 		<p class="font-bold text-indigo-300">{col_1}</p>
-		<p class="text-slate-100 italic">{col_2}</p>
+		<p class="hidden md:block text-slate-100 italic">{col_2}</p>
 		<p>{col_3}</p>
 	</div>
 </li>
