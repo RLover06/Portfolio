d26fc0c6fdee67cdefb73bb1fa32beb3a1d3a562
Hunter Stevens <hunter@toplobster.io>
2024-12-05 12:35:28 -0500

    refactor: contact

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,8 +1,21 @@
+<script>
+	const positions = [
+		// { name: 'Full-Time Positions' },
+		// { name: 'Part-Time Positions' },
+		{ name: 'Contract Positions' },
+		{ name: 'Consulting Positions' },
+	];
+</script>
+
 <div id="contact">
 	<div class="pt-12 sm:pt-16 lg:pt-20">
 		<div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
 			<div class="text-center">
-				<h2 class="text-3xl leading-9 font-extrabold sm:text-4xl sm:leading-10 lg:text-5xl lg:leading-none text-indigo-300 glow space-grotesk">Contact</h2>
+				<h2
+					class="text-3xl leading-9 font-extrabold sm:text-4xl sm:leading-10 lg:text-5xl lg:leading-none text-indigo-300 glow space-grotesk"
+				>
+					Contact
+				</h2>
 				<p class="mt-4 text-xl leading-7 text-gray-500" />
 			</div>
 		</div>
@@ -13,57 +26,41 @@
 			<div class="absolute h-1/2 bg-indigo-200" />
 			<div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div class="max-w-lg mx-auto rounded-lg glow-box">
-					<div class=" bg-gradient-to-b from-neutral-100 to-neutral-300 px-6 py-8 lg:flex-shrink-1 lg:p-12 rounded-lg hover:outline-dotted hover:outline-4 outline-indigo-400 outline-offset-4">
-						<h3 class="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9 text-center">Feel free to reach out!</h3>
+					<div
+						class=" bg-gradient-to-b from-neutral-100 to-neutral-300 px-6 py-8 lg:flex-shrink-1 lg:p-12 rounded-lg hover:outline-dotted hover:outline-4 outline-indigo-400 outline-offset-4"
+					>
+						<h3
+							class="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9 text-center"
+						>
+							Feel free to reach out!
+						</h3>
 						<p class="mt-6 text-base leading-6 text-gray-600 text-center font-bold">
 							hunter@toplobster.io <span class=" font-semibold mx-2" />
 						</p>
 						<div class="mt-8">
 							<div class="flex items-center">
-								<h4 class="flex-shrink-0 pr-4 text-sm leading-5 tracking-wider font-semibold uppercase text-indigo-600">Available for:</h4>
+								<h4
+									class="flex-shrink-0 pr-4 text-sm leading-5 tracking-wider font-semibold uppercase text-indigo-600"
+								>
+									Available for:
+								</h4>
 								<div class="flex-1 border-t-2 border-slate-800" />
 							</div>
 							<ul class="mt-8 grid grid-cols-2 col-gap-8 row-gap-5 lg:gap-y-6">
-								<!-- <li class="flex items-start lg:col-span-1">
-									<div class="flex-shrink-0">
-										<svg class="h-5 w-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20">
-											<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-										</svg>
-									</div>
-									<p class="ml-3 text-sm leading-5 text-gray-700">Full-Time Positions</p>
-								</li>
-								<li class="flex items-start lg:col-span-1">
-									<div class="flex-shrink-0">
-										<svg class="h-5 w-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20">
-											<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-										</svg>
-									</div>
-									<p class="ml-3 text-sm leading-5 text-gray-700">Part-Time Positions</p>
-								</li> -->
-								<li class="mt-5 flex items-start lg:col-span-1 lg:mt-0">
-									<div class="flex-shrink-0">
-										<svg class="h-5 w-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20">
-											<path
-												fill-rule="evenodd"
-												d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-												clip-rule="evenodd"
-											/>
-										</svg>
-									</div>
-									<p class="ml-3 text-sm leading-5 text-gray-700">Contract Positions</p>
-								</li>
-								<li class="mt-5 flex items-start lg:col-span-1 lg:mt-0">
-									<div class="flex-shrink-0">
-										<svg class="h-5 w-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20">
-											<path
-												fill-rule="evenodd"
-												d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-												clip-rule="evenodd"
-											/>
-										</svg>
-									</div>
-									<p class="ml-3 text-sm leading-5 text-gray-700">Consulting Positions</p>
-								</li>
+								{#each positions as position}
+									<li class="mt-5 flex items-start lg:col-span-1 lg:mt-0">
+										<div class="flex-shrink-0">
+											<svg class="h-5 w-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20">
+												<path
+													fill-rule="evenodd"
+													d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+													clip-rule="evenodd"
+												/>
+											</svg>
+										</div>
+										<p class="ml-3 text-sm leading-5 text-gray-700">{position.name}</p>
+									</li>
+								{/each}
 							</ul>
 						</div>
 					</div>
