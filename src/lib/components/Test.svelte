f954d3251aa52669ccf2e8ce34b89932293173b3
Hunter Stevens <hunter@toplobster.io>
2024-12-09 01:32:39 -0500

    fix: tsconfig

---
+++ src/lib/components/Test.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let inputValue: string = '';
+</script>
+
+<!-- Input and display the value -->
+<input 
+	type="text" 
+	placeholder="Type something..." 
+	oninput={(e) => {
+		e.target.value
+	}}  
+	/>
+<p>You typed: {inputValue}</p>
