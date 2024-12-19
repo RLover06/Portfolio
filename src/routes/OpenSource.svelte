3b7650ed051e9db3bab5ff3fceb8546a6055ea84
Hunter Stevens <hunter@toplobster.io>
2024-12-19 18:52:07 -0500

    chore: add svelte-shadcn-ui to OSS

--- src/routes/OpenSource.svelte
+++ src/routes/OpenSource.svelte
@@ -7,13 +7,18 @@
 		{
 			name: 'Supabase',
 			linkURL: 'https://supabase.com/',
-			imgURL: 'supabase.png'
+			imgURL: 'supabase.png',
+		},
+		{
+			name: 'Shadcn UI, Svelte',
+			linkURL: 'https://www.shadcn-svelte.com/',
+			imgURL: 'shadcn-ui.png',
 		},
 		{
 			name: 'XYFlow',
 			linkURL: 'https://xyflow.com/',
-			imgURL: 'xy-flow.png'
-		}
+			imgURL: 'xy-flow.png',
+		},
 	];
 
 	onMount(() => {
@@ -25,28 +30,28 @@
 	});
 </script>
 
-<div class="pt-4 pb-1 my-24" id="open-source">
+<div class="my-24 pb-1 pt-4" id="open-source">
 	<div class="text-center">
 		<h2
 			id="open-source-title"
-			class="text-3xl font-bold tracking-tight text-center sm:text-4xl bg-gradient-to-r text-indigo-300 glow space-grotesk"
+			class="glow space-grotesk bg-gradient-to-r text-center text-3xl font-bold tracking-tight text-indigo-300 sm:text-4xl"
 		>
 			Open Source Contributions
 		</h2>
 		<p class="mt-2 text-lg leading-8 text-gray-400">I'm always happy to contribute where I can!</p>
-			<div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
-				<ul class="flex gap-4 justify-center">
-					{#each contributions as contribution}
-						<li id="open-source-item">
-							<Icon
-								name={contribution.name}
+		<div class="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+			<ul class="flex justify-center gap-4">
+				{#each contributions as contribution}
+					<li id="open-source-item">
+						<Icon
+							name={contribution.name}
 							linkURL={contribution.linkURL}
-								imgURL={contribution.imgURL}
-							/>
-						</li>
-					{/each}
-				</ul>
-				<div id="open-source-end"></div>
+							imgURL={contribution.imgURL}
+						/>
+					</li>
+				{/each}
+			</ul>
+			<div id="open-source-end"></div>
 		</div>
 	</div>
 </div>
